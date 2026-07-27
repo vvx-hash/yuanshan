b5e4a4cf7ddcd5481347854f703de8bbc2657b4e
vvx-hash <vvx-hash@users.noreply.github.com>
2026-07-27 10:57:47 +0000

    feat: 更新 player 移动脚本，使用 get_action_strength

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,10 @@
 extends CharacterBody2D
 
-const SPEED: float = 200.0
+@export var speed := 180.0
 
-func _physics_process(delta: float) -> void:
-	var direction: Vector2 = Vector2.ZERO
-	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
-	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
-	velocity = direction.normalized() * SPEED
+func _physics_process(delta):
+	var dir = Vector2.ZERO
+	dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	velocity = dir.normalized() * speed
 	move_and_slide()
